7bbbd01e26774fea079003998af577696efab7c4
NachikethRamesh <nrames609@gmail.com>
2025-09-04 00:14:32 -0400

    Delete functions directory

--- functions/_middleware.js
+++
@@ -1,8 +0,0 @@
-// Cloudflare Pages Functions middleware
-// This file is needed to tell Cloudflare this is a Pages project, not Workers
-
-export async function onRequest(context) {
-  // For now, just pass through all requests to static files
-  // In the future, this could handle API routes
-  return await context.next();
-}
